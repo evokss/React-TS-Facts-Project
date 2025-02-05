0fc681fee540092b317de67a1de6431acc56859f
Eva <ekoss.prog@gmail.com>
2025-02-05 16:40:19 +0400

    chore: update navbar logo text from ReactFacts to ReactTSFacts

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar(): JSX.Element {
         className="nav--icon" 
         alt="React Icon"
       />
-      <h3 className="nav--logo_text">ReactFacts</h3>
+      <h3 className="nav--logo_text">ReactTSFacts</h3>
       <h4 className="nav--title">Eva's Projects</h4>
     </nav>
   );
